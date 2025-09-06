9240ace631bcde03c67b15e4b07751e7fae21cf7
Sanchari Ghosh <gsanchari9870@gmail.com>
2025-09-07 01:40:44 +0530

    update

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -51,7 +51,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, signup, logout, loading }}>
+    <AuthContext.Provider value={{ user, login,setUser, signup, logout, loading }}>
       {children}
     </AuthContext.Provider>
   );
